9ef5a8b2bfe5e81f1f8e965c244dbb5b308784fb
Tony Layher <layhertony@gmail.com>
2014-11-23 13:55:04 -0500

    hlte-common:  Clean up init
    Change-Id: Id6ccb36f4dbf8b8ed9a5542ba3eb555d5ece9e4b

--- system_prop.mk
+++ system_prop.mk
@@ -26,6 +26,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0
 
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.nfc.port=I2C
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=true \
@@ -37,3 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sensors=1
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
