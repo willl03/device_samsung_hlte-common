886c7d6ff55d4a5a0bf39a60be26810f6105f6be
willl0344 <wgangers@gmail.com>
2014-11-24 10:14:23 -0500

    Revert "Orientation sensor work without debug now"
    This reverts commit 60074503c76bba43573b42a4f8cc0ea51a4581b1.

--- system_prop.mk
+++ system_prop.mk
@@ -32,4 +32,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.add_power_save=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.lte_vrat_report=1 \
-    persist.radio.mode_pref_nv10=1
+    persist.radio.mode_pref_nv10=1 \
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sensors=1
